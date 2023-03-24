938dae29f18573e5fa53ffbb1190918c8041800a
Aishurajmanya <104428667+Aishurajmanya@users.noreply.github.com>
2023-03-24 10:44:43 +0530

    Add files via upload

---
+++ ABC.java
@@ -0,0 +1,16 @@
+//Write a Java Program to find the number is odd or even
+class ABC
+{
+ public static void main(String args[])
+ {
+   int num=5;
+   if(num % 2== 0)
+   {
+     System.out.println("Even");
+	}
+	else
+	{
+	   System.out.println("odd");
+	}
+}
+}
